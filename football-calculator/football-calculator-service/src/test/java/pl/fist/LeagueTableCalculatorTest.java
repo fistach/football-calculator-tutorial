3279b69d454ec75c99bde816d1d515e02fb68dd5
fistach <fistach@gmail.com>
2026-05-24 16:16:08 +0200

    add test to add 3 points to a winning team

--- football-calculator/football-calculator-service/src/test/java/pl/fist/LeagueTableCalculatorTest.java
+++ football-calculator/football-calculator-service/src/test/java/pl/fist/LeagueTableCalculatorTest.java
@@ -35,4 +35,29 @@ public class LeagueTableCalculatorTest {
         assertEquals(2, result.size());
         assertEquals(new HashSet<>(teams), new HashSet<>(result));
     }
+
+    @Test
+    void shouldAdd3PointsToTheWinningTeam() {
+        //given
+        TeamRepository teamRepository = mock(TeamRepository.class);
+        MatchRepository matchRepository = mock(MatchRepository.class);
+
+        List<Team> teams = List.of(new Team("FC club", 0),
+                new Team("Club 2", 3));
+
+        when(teamRepository.findAll()).thenReturn(teams);
+
+        List<Match> matches = List.of(new Match(teams.get(0), teams.get(1), 3, 1));
+
+        when(matchRepository.findAll()).thenReturn(matches);
+        UpdateLeagueTableService service = new UpdateLeagueTableService(teamRepository,
+                matchRepository);
+
+        //when
+        List<Team> result = service.calculate();
+
+        //then
+        assertEquals(3, result.get(0).points);
+        assertEquals(3, result.get(1).points);
+    }
 }
